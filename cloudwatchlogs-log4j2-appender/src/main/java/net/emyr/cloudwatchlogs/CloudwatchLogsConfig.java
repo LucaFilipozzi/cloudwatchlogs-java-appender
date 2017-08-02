35337abc79474b133b7c69563c6a3175fc2886d7
Debian <admin@ip-172-16-0-47.ca-central-1.compute.internal>
2017-08-02 17:14:04 +0000

    clean up: package, remove logback & large test

---
+++ cloudwatchlogs-log4j2-appender/src/main/java/net/emyr/cloudwatchlogs/CloudwatchLogsConfig.java
@@ -0,0 +1,218 @@
+package net.emyr.cloudwatchlogs;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+/**
+ * General configuration for the CloudWatch appender.
+ */
+public class CloudwatchLogsConfig {
+    /**
+     * The default size of the async log event queue.
+     */
+    public static final int DEFAULT_MAX_EVENT_QUEUE_SIZE = 1000000;
+
+    private int maxEventQueueSize = DEFAULT_MAX_EVENT_QUEUE_SIZE;
+
+    private boolean debug;
+    private String endpoint = System.getenv("BOXFUSE_CLOUDWATCHLOGS_ENDPOINT");
+    private String env = System.getenv("BOXFUSE_ENV");
+    private String image = System.getenv("BOXFUSE_IMAGE_COORDINATES");
+    private String instance = System.getenv("BOXFUSE_INSTANCE_ID");
+    private String region = System.getenv("AWS_REGION");
+
+    private boolean stdoutFallback;
+
+    public CloudwatchLogsConfig() {
+        if (env == null) {
+            env = "##unknown##";
+        }
+        if (image == null) {
+            image = "##unknown##:##unknown##";
+        }
+        if (instance == null) {
+            instance = getHostName();
+        }
+    }
+
+    /**
+     * @return Whether to fall back to stdout instead of disabling the appender when running outside of a Boxfuse instance. Default: false.
+     */
+    public boolean isStdoutFallback() {
+        return stdoutFallback;
+    }
+
+    /**
+     * @param stdoutFallback Whether to fall back to stdout instead of disabling the appender when running outside of a Boxfuse instance. Default: false.
+     */
+    public void setStdoutFallback(boolean stdoutFallback) {
+        this.stdoutFallback = stdoutFallback;
+    }
+
+    /**
+     * @return The maximum size of the async log event queue. Default: 1000000.
+     * Increase to avoid dropping log events at very high throughput.
+     * Decrease to reduce maximum memory usage at the risk if the occasional log event drop when it gets full.
+     */
+    public int getMaxEventQueueSize() {
+        return maxEventQueueSize;
+    }
+
+    /**
+     * @param maxEventQueueSize The maximum size of the async log event queue. Default: 1000000.
+     *                          Increase to avoid dropping log events at very high throughput.
+     *                          Decrease to reduce maximum memory usage at the risk if the occasional log event drop when it gets full.
+     */
+    public void setMaxEventQueueSize(int maxEventQueueSize) {
+        if (maxEventQueueSize < 1) {
+            throw new IllegalArgumentException("maxEventQueueSize may not be smaller than 1 but was " + maxEventQueueSize);
+        }
+        this.maxEventQueueSize = maxEventQueueSize;
+    }
+
+    /**
+     * @return The AWS CloudWatch Logs endpoint to connect to.
+     */
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    /**
+     * @param endpoint The AWS CloudWatch Logs endpoint to connect to.
+     */
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    /**
+     * @return The current Boxfuse environment.
+     */
+    public String getEnv() {
+        return env;
+    }
+
+    /**
+     * @param env The current Boxfuse environment.
+     */
+    public void setEnv(String env) {
+        if (env == null) {
+            throw new IllegalArgumentException("env may not be null");
+        }
+        this.env = env;
+    }
+
+    /**
+     * @return The current Boxfuse image.
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * @param image The current Boxfuse image.
+     */
+    public void setImage(String image) {
+        if (image == null) {
+            throw new IllegalArgumentException("image may not be null");
+        }
+        this.image = image;
+    }
+
+    /**
+     * @return The id of the current instance.
+     */
+    public String getInstance() {
+        return instance;
+    }
+
+    /**
+     * @param instance The id of the current instance.
+     */
+    public void setInstance(String instance) {
+        if (instance == null) {
+            throw new IllegalArgumentException("instance may not be null");
+        }
+        this.instance = instance;
+    }
+
+    /**
+     * @return The AWS region to use.
+     */
+    public String getRegion() {
+        return region;
+    }
+
+    /**
+     * @param region The AWS region to use.
+     */
+    public void setRegion(String region) {
+        if (region == null) {
+            throw new IllegalArgumentException("region may not be null");
+        }
+        this.region = region;
+    }
+
+    /**
+     * @return Whether internal debug output should produced. Only useful for diagnosing issues within the appender itself.
+     */
+    public boolean isDebug() {
+        return debug;
+    }
+
+    /**
+     * @param debug Whether internal debug output should produced. Only useful for diagnosing issues within the appender itself.
+     */
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
+
+    private static String getHostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException var1) {
+            return getHostIp();
+        }
+    }
+
+    private static String getHostIp() {
+        try {
+            Enumeration e = NetworkInterface.getNetworkInterfaces();
+            String backupCandidate = null;
+
+            while (true) {
+                NetworkInterface networkInterface;
+                do {
+                    if (!e.hasMoreElements()) {
+                        if (backupCandidate != null) {
+                            return backupCandidate;
+                        }
+
+                        return "<<unknown>>";
+                    }
+
+                    networkInterface = (NetworkInterface) e.nextElement();
+                } while (!networkInterface.isUp());
+
+                boolean firstChoice = !networkInterface.getName().contains("vboxnet") && !networkInterface.getName().contains("vmnet") && (networkInterface.getDisplayName() == null || !networkInterface.getDisplayName().contains("VirtualBox") && !networkInterface.getDisplayName().contains("VMware"));
+                Enumeration inetAddresses = networkInterface.getInetAddresses();
+
+                while (inetAddresses.hasMoreElements()) {
+                    InetAddress inetAddress = (InetAddress) inetAddresses.nextElement();
+                    if (inetAddress instanceof Inet4Address && !inetAddress.isLoopbackAddress()) {
+                        if (firstChoice) {
+                            return inetAddress.getHostAddress();
+                        }
+
+                        backupCandidate = inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException var6) {
+            return "<<unknown>>";
+        }
+    }
+}
